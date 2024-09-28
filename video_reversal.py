dd43c2e416457bc7ee5f231cda424d27a60fe760
TalhaFaisalCh <talhafaisalch@gmail.com>
2024-09-28 13:43:28 +0500

    Program to reverse a video file or camera input

---
+++ video_reversal.py
@@ -0,0 +1,50 @@
+#!/usr/bin/env python3
+import cv2
+import numpy as np
+
+choice = input("reverse the video?[Y/N]: ").upper()
+
+# Open the default camera
+#cam = cv2.VideoCapture('/Users/faisalshafait/Documents/PythonCode/output.mp4')
+cam = cv2.VideoCapture(0)
+
+# Get the default frame width and height
+frame_width = int(cam.get(cv2.CAP_PROP_FRAME_WIDTH))
+frame_height = int(cam.get(cv2.CAP_PROP_FRAME_HEIGHT))
+new_width = int(input('Width >>>'))
+new_height = int(input('Height >>>'))
+# Define the codec and create VideoWriter object
+fourcc = cv2.VideoWriter_fourcc(*'mp4v')
+out = cv2.VideoWriter('/Users/faisalshafait/Documents/PythonCode/NEWoutput.mp4', 
+                      fourcc, 20.0, (new_width, new_height))
+print(frame_height, frame_width)
+list_frame = []
+while True:
+    ret, frame = cam.read()
+    resized_image = cv2.resize(frame, (new_width, new_height))
+    list_frame.append(resized_image)
+
+    # Display the captured frame
+    cv2.imshow('Talha\'s livestream', resized_image)
+
+    # Press 'q' to exit the loop
+    if cv2.waitKey(1) == ord('q'):
+        break
+
+nFrames = len(list_frame)
+start = 10
+end = nFrames - 10
+step = 1
+reverse = (choice == 'Y')
+if reverse:
+    start = nFrames - 10
+    end = 10
+    step = -1
+for i in range(start, end, step):
+    out.write(list_frame[i])
+
+
+# Release the capture and writer objects
+cam.release()
+out.release()
+cv2.destroyAllWindows()
