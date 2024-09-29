0720275f44e30c62a92795e39d2e7cc2ed04aaa8
TalhaFaisalCh <talhafaisalch@gmail.com>
2024-09-29 14:14:44 +0500

    Added grayscale video feature

--- video_reversal.py
+++ video_reversal.py
@@ -16,16 +16,17 @@ new_height = int(input('Height >>>'))
 # Define the codec and create VideoWriter object
 fourcc = cv2.VideoWriter_fourcc(*'mp4v')
 out = cv2.VideoWriter('/Users/faisalshafait/Documents/PythonCode/NEWoutput.mp4', 
-                      fourcc, 20.0, (new_width, new_height))
+                      fourcc, 30.0, (new_width, new_height), isColor=False)
 print(frame_height, frame_width)
 list_frame = []
 while True:
     ret, frame = cam.read()
     resized_image = cv2.resize(frame, (new_width, new_height))
-    list_frame.append(resized_image)
+    gray_image = cv2.cvtColor(resized_image, cv2.COLOR_BGR2GRAY)
+    list_frame.append(gray_image)
 
     # Display the captured frame
-    cv2.imshow('Talha\'s livestream', resized_image)
+    cv2.imshow('Talha\'s livestream', gray_image)
 
     # Press 'q' to exit the loop
     if cv2.waitKey(1) == ord('q'):
