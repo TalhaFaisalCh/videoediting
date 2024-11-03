50a514c35a7e11356d073019fa947a0cd79bddb0
TalhaFaisalCh <talhafaisalch@gmail.com>
2024-11-03 13:19:00 +0500

    Update video_reversal.py

--- video_reversal.py
+++ video_reversal.py
@@ -1,41 +1,55 @@
 #!/usr/bin/env python3
 import cv2
 import numpy as np
+import os
 
+
+# strictly say 'y'(doesn't matter if its capital or not) if you want to video reverse 
 choice = input("reverse the video?[Y/N]: ").upper()
 
 # Open the default camera
-#cam = cv2.VideoCapture('/Users/faisalshafait/Documents/PythonCode/output.mp4')
 cam = cv2.VideoCapture(0)
-
-# Get the default frame width and height
+# To get grayscale, change the variable coloredvideo to False below
+coloredvideo = True
+# Get the default frame width and height (CAP = capture)(PROP = property)
 frame_width = int(cam.get(cv2.CAP_PROP_FRAME_WIDTH))
 frame_height = int(cam.get(cv2.CAP_PROP_FRAME_HEIGHT))
-new_width = int(input('Width >>>'))
-new_height = int(input('Height >>>'))
-# Define the codec and create VideoWriter object
+# If you want to enable change size, replace frame_width and frame_height with the program infront of it and enable it by removing '#'
+new_width = frame_width #int(input('Width >>>'))
+new_height = frame_height #int(input('Height >>>'))
+# Define the codec and create VideoWriter object (fourcc == Four character code) 
 fourcc = cv2.VideoWriter_fourcc(*'mp4v')
-out = cv2.VideoWriter('/Users/faisalshafait/Documents/PythonCode/NEWoutput.mp4', 
-                      fourcc, 30.0, (new_width, new_height), isColor=False)
-print(frame_height, frame_width)
+# If you want to specify a path, add to filename.mp4 e.g D:\folder\filename.mp4
+out = cv2.VideoWriter('pythonvideo.mp4', 
+                      fourcc, 30.0, (new_width, new_height), isColor=coloredvideo)
+
+print(new_width, new_height)
 list_frame = []
 while True:
     ret, frame = cam.read()
+    if ret == False:
+        print("Camera reading failed, know why?")
+        quit()
     resized_image = cv2.resize(frame, (new_width, new_height))
-    gray_image = cv2.cvtColor(resized_image, cv2.COLOR_BGR2GRAY)
-    list_frame.append(gray_image)
+    # If you want to have grayscale video, look above
+    if coloredvideo == False:
+        resized_image = cv2.cvtColor(resized_image, cv2.COLOR_BGR2GRAY)
+    list_frame.append(resized_image)
 
     # Display the captured frame
-    cv2.imshow('Talha\'s livestream', gray_image)
+    cv2.imshow('Video', resized_image)
 
     # Press 'q' to exit the loop
     if cv2.waitKey(1) == ord('q'):
         break
 
+# nFrames = number frames
 nFrames = len(list_frame)
 start = 10
+# Cuts out the part where camera shuts down, it is obvious if you remove '-10'
 end = nFrames - 10
 step = 1
+# (if the input choice is 'Y', then is true, so (choice == 'Y') = True. Else it will be false)
 reverse = (choice == 'Y')
 if reverse:
     start = nFrames - 10
@@ -43,6 +57,9 @@ if reverse:
     step = -1
 for i in range(start, end, step):
     out.write(list_frame[i])
+
+# If you want to have a specific path, go to line 22 and disable the print statement below with '#' at beginning
+print('Video stored at: ', os.getcwd())
 
 
 # Release the capture and writer objects
